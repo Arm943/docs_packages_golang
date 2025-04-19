5a03ecdfc831dd90ebe3f95006bb6ed109fb35ba
ArmenTs <stalin943@gmail.com>
2025-04-19 19:19:32 +0300

    практика основных методов пакета strconv

--- main.go
+++ main.go
@@ -2,35 +2,47 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"log"
+	"strconv"
 )
 
-func demo1(a string) {
-	b := strings.ReplaceAll(a, "Go", "Golang")
-	fmt.Println(b)
+func demo1() {
+	result := strconv.Itoa(123)
+	fmt.Println(result)
 }
+
 func demo2() {
-	a := strings.NewReplacer("&", "и", "<", "меньше", ">", "больше")
-	fmt.Println(a.Replace("5 < 10 & 10 > 5"))
+	result, err := strconv.Atoi("456")
+	if err != nil {
+		log.Println("Ошибка в конвертации: ", err)
+		return
+	}
+
+	fmt.Println(result + 100)
 }
 
 func demo3() {
-	var builder strings.Builder
-	builder.WriteString("Hello")
-	builder.WriteString(" from")
-	builder.WriteString(" Builder")
-	fmt.Println(builder.String())
+	a := strconv.FormatInt(31, 16)
+	fmt.Println(a)
+}
+func demo4() {
+	a, err := strconv.ParseInt("1111", 2, 16)
+	if err != nil {
+		log.Println("Ошибка конвертации числа", err)
+		return
+	}
+	fmt.Println(a)
 }
 
-func demo4() {
-	text := "go is great"
-	title := strings.Title(text)
-	fmt.Println(title)
+func demo5() {
+	a := strconv.FormatFloat(3.14159, 'f', 2, 64)
+	fmt.Println(a)
 }
 
 func main() {
-	demo1("I love Go. Go is powerful. Go is fun.")
+	demo1()
 	demo2()
 	demo3()
 	demo4()
+	demo5()
 }
